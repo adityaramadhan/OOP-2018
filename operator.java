0811c578d5a681386961009c26369a05d2209a18
adityaramadhan <32738303+adityaramadhan@users.noreply.github.com>
2018-01-08 22:13:12 +0700

    Add files via upload

---
+++ operator.java
@@ -0,0 +1,29 @@
+public class operator{
+	
+	private int x;
+	private int y;
+
+}
+
+public String setAngka( int x, int y){
+	this.x=x;
+	this.y=y;
+
+}
+
+public String getAngka( int x, int y){
+	return this.x;
+	return this.y;
+}
+
+public class calculator{
+	public int tambah( int x, int y){
+		return x + y;
+
+	}
+	public int kurang( int x, int y){
+
+		return x- y;
+	}
+
+}
